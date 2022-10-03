28363f8610ab6b0778b6cbe41de747a1128a5ce9
TheNeverMan <thenevermanever@gmail.com>
2022-10-03 22:30:43 +0200

    1.2.9

--- gtk_tile.cpp
+++ gtk_tile.cpp
@@ -31,8 +31,12 @@ void Gtk_Tile::Set_City_Name(string city_name)
   City_Name_Label.set_text(city_name);
   City_Name_Label.set_halign(Gtk::ALIGN_CENTER);
   City_Name_Label.set_valign(Gtk::ALIGN_END);
+//City_Name_Label.set_width_chars(50);
+//  City_Name_Label.set_hexpand(true);
   Gtk::Label& ref = City_Name_Label;
   City_Name_Overlay.add_overlay(static_cast<Gtk::Widget&>(ref));
+  City_Name_Overlay.set_overlay_pass_through(static_cast<Gtk::Widget&>(ref), false);
+  //City_Name_Overlay.set_size_request(City_Name_Label.get_width(), tile_size);
   City_Name_Label.show();
 }
 
