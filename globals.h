40b9fbbfa79cdcfe8d48b20fdcadff5c1f2ebb96
TheNeverMan <thenevermanever@gmail.com>
2022-10-06 20:39:31 +0200

    1.2.16

--- globals.h
+++ globals.h
@@ -6,7 +6,7 @@ using namespace std;
 //version of the game
 const int version_major = 1;
 const int version_minor = 2;
-const int version_build = 15;
+const int version_build = 16;
 const string codename="FLAGS";
 const string assets_directory_path = "assets/";
 const string path_delimeter = "/";
