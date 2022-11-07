b63273ac29d1100c4c3bddd44328d2f37a4129fb
TheNeverMan <thenevermanever@gmail.com>
2022-11-07 21:00:26 +0100

    1.3.18

--- globals.h
+++ globals.h
@@ -6,7 +6,7 @@ using namespace std;
 //version of the game
 const int version_major = 1;
 const int version_minor = 3;
-const int version_build = 17;
+const int version_build = 18;
 const string codename="AUDIO";
 
 string Display_Version();
